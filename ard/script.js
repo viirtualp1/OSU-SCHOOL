45cf5b0d1f448f76e230c4577b3bb4706b27e597
Nikita <zininnikita309@gmail.com>
2022-04-05 23:32:08 +0900

    create math examples

--- ard/script.js
+++ ard/script.js
@@ -2,32 +2,62 @@ const getRandId = (min, max) => Math.floor(Math.random() * (max - min)) + min
 
 const game = document.querySelector("#game");
 
-createCircle()
+const mathExamples = [
+    { example: '2 + 2', answer: 4 },
+    { example: '3 + 5', answer: 8 },
+    { example: '6 + 3', answer: 9 },
+    { example: '2 * 3', answer: 6 },
+    { example: '1 * 4', answer: 4 },
+    { example: '7 - 2', answer: 5 },
+    { example: '6 - 3', answer: 3 },
+]
 
-function createCircle() {
+
+function init() {
+    mathExampleCreate = mathExample();
+
+    createCircle([mathExampleCreate.answer - getRandId(1, 3), mathExampleCreate.answer + getRandId(1, 2), mathExampleCreate.answer])
+    checkAnswer(mathExampleCreate.answer);
+}
+
+init();
+
+function checkAnswer(correctAnswer) {
+    for (let i = 0; i < 3; i++) {
+        const circle = document.querySelector(`#circle${i}`);
+
+        circle.addEventListener("click", () => {
+            circle.childNodes[0].id == correctAnswer ? alert('Правильно!') : alert('Неправильно!')
+
+            location.reload();
+        });
+    }
+}
+
+function mathExample() {
+    example = mathExamples[getRandId(0, mathExamples.length)];
+    document.querySelector('#math-example').innerHTML = `${example.example} = ?`;
+
+    return example
+}
+
+function createCircle(circleNumber = [num1, num2, num3]) {
     for (let i = 0; i < 3; i++) {
         const circle = document.createElement("div");
         const img = document.createElement("img");
-        const img2 = document.createElement("img");
         
-        const randNumb = getRandId(0, 9)
-
-        img.src = `default-${randNumb}.png`;
-        circle.appendChild(img);
-
         const randIdLeft = getRandId(0, 1000);
         const randIdTop = getRandId(0, 200);
+
+        img.src = `default-${circleNumber[i]}.png`;
+        img.id = circleNumber[i];
+        circle.appendChild(img);
     
-        circle.id = "circle";
+        circle.className = `circle`;
+        circle.id = `circle${i}`;
         circle.style.marginLeft = `${randIdLeft}px`
         circle.style.marginTop = `${randIdTop}px`
     
         game.appendChild(circle);
-    
-        circle.addEventListener("click", () => {
-            circle.parentNode.removeChild(circle);
-    
-            createCircle();
-        })
     }
 }
