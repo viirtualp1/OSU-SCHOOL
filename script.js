25de560ff6aaff72cdff532f420584262e2cc48e
Nikita <zininnikita309@gmail.com>
2022-04-25 09:23:55 +0300

    big update
    - added progress bar (timer)
    - start add comments in code

--- script.js
+++ script.js
@@ -1,15 +1,33 @@
+// фукнция для генерации числа от min до max чисел 
 const getRandId = (min, max) => Math.floor(Math.random() * (max - min)) + min
 
+// получаем див, где будет происходить игра)
 const game = document.querySelector("#game");
 
+// макс. диапазон чисел для создания примера
 let maxRange = 20;
+
+// счетчик комбо
 let streakCounter = 0;
 
+// здесь будет храниться таймер до перезагрузки страницы, если юзер не ответил на пример за 5 сек
+let timerToLose;
+
+// здесь будет храниться интервал, для обновления прогресс бара
+let timerUpdateProgressBar;
+
+// начальная ширина прогресс бара
+let widthProgress = 100;
+
+// Запускаем игру
 init();
 
-function addRange() { maxRange += 5; init() }
-function decreaseRange() { maxRange > 20 ? maxRange -= 5 : 20; init() }
+// увеличить диапазон чисел при создании примера
+function addRange() { maxRange += 5; reloadGame(false) }
+// уменьшить диапазон чисел при создании примера
+function decreaseRange() { maxRange > 20 ? maxRange -= 5 : 20; reloadGame(false) }
 
+// перемешать массив с числами, которые будут в circles (варианты ответов на пример)
 function shuffle(array) {
     let currentIndex = array.length,  randomIndex;
   
@@ -23,6 +41,7 @@ function shuffle(array) {
     return array;
 }
 
+// создаем 2 числа из которых будет строиться пример
 function randNumbers() {
     let numbers = [];
 
@@ -31,38 +50,57 @@ function randNumbers() {
     return numbers
 }
 
+// ининицализация игры 
 function init() {
+    // удаление пред. circles
     clearCircles();
-    let numbers = [];
 
+    // массив для вариантов ответов (circles)
+    let numbers = [];
     for (let i = 0; i < 4; i++) numbers.push(randNumbers());
 
+    // массив с примерами (на вычитание и сложение) 
     const mathExamples = [
         { example: `${numbers[0][0]} + ${numbers[0][1]}`, answer: numbers[0][0] + numbers[0][1]},
         { example: `${numbers[3][0]} - ${numbers[3][1]}`, answer: numbers[3][0] - numbers[3][1]},
     ]
 
+    // создание примера
     mathExampleCreate = mathExample(mathExamples);
 
+    // создание circles, два рандомных числа (ответ - рандЧисло(1, 10), ответ + рандЧисло(1, 20), правильный ответ)
     createCircle([mathExampleCreate.answer - getRandId(1, 10), mathExampleCreate.answer + getRandId(1, 20), mathExampleCreate.answer])
+    // добавление функции при клике на circle и проверка на совпадение с правильным ответом
     checkAnswer(mathExampleCreate.answer);
 
+    // вставка макс. диапазона для создания примера
     document.querySelector('#maxRangeText').innerHTML = `Макс. диапазон: ${maxRange}`;
+
+    // создание таймера на решение примера
+    initTimer();
 }
 
+// добавление функции при клике на circle и проверка ответа
 function checkAnswer(correctAnswer) {
+    // перебор каждого из circle
     for (let i = 0; i < 3; i++) {
+        // получение текущего circle
         const circle = document.querySelector(`#circle${i}`);
 
+        // добавление ему события click
         circle.addEventListener("click", () => {
+            // в Id добавлено число в circle, его мы сравниваем с правильным ответом на пример и вызываем уведомление 
             circle.childNodes[0].id == correctAnswer ? notification('Правильно!', true) : notification('Неправильно!', false)
 
-            init();
+            // перезагружаем игру
+            reloadGame(false);
         });
     }
 }
 
+// уведомления (правильно/нет)
 function notification(text, isCorrect) {
+    // тоаст (sweetalert2)
     const Toast = Swal.mixin({
         toast: true,
         position: 'top-start',
@@ -75,21 +113,29 @@ function notification(text, isCorrect) {
         }
     })
     
+    // иконка и текст toast
     Toast.fire({
         icon: isCorrect ? 'success' : 'error',
         title: text
     })
 
+    // вызов функции стрик
     streak(isCorrect);
 }
 
+// стрик
 function streak(isCorrect) {
+    // если isCorrect == true, то мы добавляем к счетчику комбо +1, иначе присваимаем 0
     streakCounter = isCorrect ? streakCounter += 1 : 0;
+    // обновляем текст счетчика комбо
     document.querySelector("#streak-text").innerHTML = `Комбо: ${streakCounter}`;
 }
 
+// функция создания примера
 function mathExample(numbers) {
+    // выбираем один из двух рандомных примеров
     example = numbers[getRandId(0, numbers.length)];
+    // вставляем его в 
     document.querySelector('#math-example').innerHTML = `${example.example} = ?`;
  
     return example
@@ -121,4 +167,23 @@ function createCircle(circleNumber = [num1, num2, num3]) {
 
 function clearCircles() {
     game.innerHTML = ``;
+}
+
+function initTimer() {
+    timerUpdateProgressBar = setInterval(() => {
+        document.querySelector("#progress-bar-timer").style.width = `${widthProgress -= 20}%`
+    }, 1000);
+
+    timerToLose = setTimeout(() => reloadGame(true), 6000);
+}
+
+function reloadGame(lose) {
+    clearTimeout(timerToLose);
+    clearInterval(timerUpdateProgressBar);
+    widthProgress = 100;
+    document.querySelector("#progress-bar-timer").style.width = `100%`
+
+    lose ? streak(false) : null;
+
+    init();
 }
